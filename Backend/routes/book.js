2eaa509139d67b0c342144a23387e9ced90516fe
daiba <daiba_b@yahoo.com>
2023-12-01 14:38:44 +0100

    Finalisation

--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -5,13 +5,13 @@ const multer = require("../middleware/multer-config");
 const compressImage = require("../middleware/sharp-config");
 const bookCtrl = require("../controllers/book");
 
+router.get("/bestrating", bookCtrl.bestRating);
 router.get("/", bookCtrl.getAllBook);
 router.get("/:id", bookCtrl.getOneBook);
 router.post("/", auth, multer, compressImage, bookCtrl.createBook);
 router.put("/:id", auth, multer, compressImage, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
-router.get("/bestrating", bookCtrl.bestRating);
 router.post("/:id/rating", auth, bookCtrl.averageRating);
 
 module.exports = router;
